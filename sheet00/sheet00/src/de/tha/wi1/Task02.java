1adcd58fd7ca50bc0132eebb7dc52d6d57b92b9d
heismessan <samimessan04@gmail.com>
2026-05-30 22:22:09 +0000

    exercice 2

---
+++ sheet00/sheet00/src/de/tha/wi1/Task02.java
@@ -0,0 +1,13 @@
+package de.tha.wi1;
+
+public class Task02 {
+    public static void main(String[] args){
+        System.out.println("        _\" \"_");
+        System.out.println("       .        .");
+        System.out.println("    /[]   _  _\\");
+        System.out.println("    _ | _o_LII | _");
+        System.out.println("  /   |  ====  |  \\");
+        System.out.println("  |   |  ====  | _\\");
+        System.out.println("");
+    }
+}
