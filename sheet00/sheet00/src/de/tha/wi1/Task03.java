602412bba9f3eda5b305b205d39b5ac2918ca9f4
heismessan <samimessan04@gmail.com>
2026-06-03 21:55:20 +0000

    Exercice 3 débuter

---
+++ sheet00/sheet00/src/de/tha/wi1/Task03.java
@@ -0,0 +1,26 @@
+// Exercice 3
+package de.tha.wi1;
+public class Task03
+{
+    public static void main(String[] args)
+    {
+        // question a
+        System.out.println("Hello World " + 25 + " " + 25);
+        System.out.println(25 + " " +25 + " Hello World");
+        System.out.println(25 + " Hello World " + 25);
+        System.out.println((25 + 25) + " Hello World " + 25 + " Salut les gens!");
+        System.out.println((25 + " "+ 25) + " Hello World " + 25 + " Salut les gens!");
+        
+        
+        // question b
+        float a = 23657 / 2;
+        float b = 1/3;
+        int c = 256521451645;
+        int d = c*c;
+        int e = 256 / 0;
+        int f = -264 / 0;
+        int g = 0/0;
+        
+
+    } 
+}
