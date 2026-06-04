7072ba230d25f9918cc5d05a3fccaff21fd91ab4
heismessan <samimessan04@gmail.com>
2026-06-04 22:41:07 +0000

    Partie 2 des exercices

---
+++ sheet01/sheet01/src/de/tha/wi1/Task01.java
@@ -0,0 +1,17 @@
+package de.tha.wi1;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		int[] tab = {80, 1110010, 111, 67, 61};
+		for(int i : tab){
+			System.out.println(
+				Integer.toBinaryString(i);
+				Integer.toOctalString(i);
+				Integer.toDecimalString(i);
+				Integer.toHexString(i);
+			);
+		}
+	}
+	
+}
