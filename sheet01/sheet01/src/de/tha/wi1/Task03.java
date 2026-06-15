8507c38072fc37a5338dd30d87f7dbce2f18b334
heismessan <samimessan04@gmail.com>
2026-06-15 20:46:39 +0000

    Exercice suivant

---
+++ sheet01/sheet01/src/de/tha/wi1/Task03.java
@@ -0,0 +1,20 @@
+package de.tha.wi1;
+
+public class Task03 {
+
+	public static void main(String[] args) {
+
+		// Teilaufgabe a)
+		int hours = 1, minutes = 2, seconds = 3;
+		
+		// Teilaufgabe b)
+		int totalSeconds = 18101;	// Entspricht den seit 0:00 Uhr vergangenen Sekunden
+		
+		// Teilaufgabe c) und d)
+		int hoursA = 14, minutesA = 20, secondsA = 33;
+		int hoursB = 18, minutesB = 31, secondsB = 43;
+//		int hoursB = 14, minutesB = 21, secondsB = 21;
+
+	}
+
+}
